518e187b0c4c0b6fecaf21688d26dbbe63b31292
Richard Hunter <richard@richardhunter.co.uk>
2015-10-25 22:55:03 +0000

    building main structure

--- index.js
+++ index.js
@@ -1,47 +1,183 @@
+
 var parse  = require('svg-path-parser');
+var xml2js = require('xml2js');
+var parseString = xml2js.parseString;
+var util = require('util');
 
-function test() {
+var comma = ',';
+var space = '\u0020';
 
-	var result = parse('M3,7 5-6 L1,7 1e2-.4 m-10,10 l10,0');
-	console.log(result);
+exports.handlePolygon = function() {
+
 
 }
-test();
-//  convert array of commands back to string
-//  surprised this isn't in svg path parser?
-exports.toPath = function(commands) {
-	//  commands is an array of commands produced by svg pathparser.
-	return '';
+exports.handleText = function () {}
 
+exports.handlePolyLine = function () {};
+
+exports.handleCircle = function () {};
+
+exports.handleRectangle = function () {};
+
+exports.handleUse = function () {};
+
+exports.handleLine = function () {};
+
+//  handle paths
+exports._handlePathElement = function (pathArray) {
+	//  extract path string form path element
+	var path = pathArray[0]['$']['d'];
+	var convertedPath = this.handlePath(path);
+	pathArray[0]['$']['d'] = convertedPath;
+}
+
+exports.handlePath = function (path) {
+	var pathCommands = parse(path);
+	//  transform coords from absolute into proportionate
+	return this.serialize(pathCommands);
+}
+
+exports.serialize = function (commands) {
+	var path = '';
+	for(var i=0; i < commands.length; i++) {
+		path += this._handleCommand(commands[i]) + space;
+	}
+	return compact(path.trim());
+}
+
+// remove superflous commas, add newlines if appropriate todo
+// hoping to use sVGO for this. Maybe.
+function compact(path) {
+	return path;
+}
+exports._handleM = function (command) {
+	return 'M' + command.x + comma + command.y;
 };
-//  http://www.w3.org/TR/SVG11/masking.html#EstablishingANewClippingPath
-exports.allowedElementTypes = [
-	'path', 'text', 'use', 'rect', 'circle', 'ellipse', 'line', 'polyline', 'polygon'
-];
+exports._handleRelativeM = function (command) {
+	return 'm' + command.x + comma + command.y;
+};
+exports._handleL = function (command) {
+	return 'L' + command.x + comma + command.y;
+};
+exports._handleRelativeL = function (command) {
+	return 'l' + command.x + comma + command.y;
+};
 
-
-
-exports._isLegalElement = function(element) {
-	return this.allowedElementTypes.indexOf(element) != -1;
+exports._handleCommand = function(command) {
+	var result;
+	switch(command.code) {
+	case 'M' :
+		// handle M
+		result = this._handleM(command);
+		break;
+	case 'm' : 
+		// handle m
+		result = this._handleRelativeM(command); 
+		break;
+	case 'L' : 
+		// handle L
+		result = this._handleL(command);
+		break;
+	case 'l' : 
+		// handle l
+		result = this._handleRelativeL(command);
+		break;
+	default:
+		throw {
+			name : 'unrecognised path code'
+		}
+	}
+	return result;
 }
+
 
 exports.printModuleName = function () {
 	console.log('this is Malebranche');
 };
 exports.name = 'malebranche';
 
-exports.convert = function (fragment, initialWidth, initialHeight) {
+exports.convertClipPath = function (clipPath) {
+	//  clipPath should be an array so throw an error if it isn't
+	if(!_isArray(clipPath)) {
+		throw {
+			name : 'clip path element badly formed'
+		};
+	}
+	for(var i=0; i < clipPath.length; i++) {
+		//  handle child elements of clipPath. 
+		//  I don't think we care about nested elements?
+		//  better check the spec!
+		//  some more defensive programming here.
+		if(!_isObject(clipPath[i])) {
+			throw {
+				name : 'clip path children should all be objects'
+			};
+		}
+		//  we are only interested in the first property within this object.
+		var childPathElementName = Object.keys(clipPath[i])[0];
+		switch(childPathElementName) {
+		case 'path':
+			this._handlePathElement(clipPath[i]);		
+			break;
+		case '':
+			break;
+		}
+	}
+};
 
-	/*
+/*
 		arguments are a string representing an svg clippath, the initial width of the box containing 
 the path and the initial height. The convert function should take all the coords specified within the path and make them proportionate to the width or height and then return the new clippath as a string.
 
 Should throw an error if the input is badly formed. the user is responsible for passing a correctly formed clippath.
 
-	*/
-
+*/
+exports.convert = function (xml, initialWidth, initialHeight) {
+	var self = this;
+	parseString(xml, function (err, result) {
+     console.log('foo',util.inspect(result, false, null));
+		forEachClipPath(result, self.convertClipPath.bind(self));
+		var builder = new xml2js.Builder();
+		var convertedXml = builder.buildObject(result);
+		console.log(convertedXml);
+	});	
+}
+//  pinched from underscore!
+function _isObject(obj) {
+  var type = typeof obj;
+  return type === 'function' || type === 'object' && !!obj;
 }
 
+
+
+function _isArray(obj) {
+  return Object.prototype.toString.call(obj) === '[object Array]';
+}
+
+function forEachClipPath(xml, callback) {
+	// find all clip paths within xml and calll call back on all of them	
+	if(_isArray(xml)) {
+		// handle as array
+		for(var i = 0; i < xml.length; i++) {
+			forEachClipPath(xml[i], callback);
+		}
+	} else if(_isObject(xml)) {
+		for(var prop in xml) {
+			if(xml.hasOwnProperty(prop)) {
+				var propertyVal = xml[prop];
+				if( prop === 'clipPath') {
+					callback(propertyVal);
+				} else {
+					forEachClipPath(propertyVal, callback);
+				}
+			}
+		}
+		//  handle as object
+	} else {
+		//  do something else.
+	}
+} 
+exports._forEachClipPath = forEachClipPath; // export so we can test
 exports.convertFile = function (svgFile) {
 	//  should take an svg file and convert all clip paths in it
 }
--- spec/main.spec.js
+++ spec/main.spec.js
@@ -1,27 +1,54 @@
-
-
 describe('Malebranche tests', function () {
-
 	var malebranche = require('../index.js');
+	describe('handlePath', function () {
+		//  todo: this should return a converted path of course
+		it('should return path', function () {
+			var path = 'M3,7 L5,-6 L1,7 L100,-0.4 m-10,10 l10,0';
+			expect(malebranche.handlePath(path)).toBe(path);
+		});
+	});
 	describe('name', function () {
 		it('should return module name', function () {
 			expect(malebranche.name).toBe('malebranche');
 		});
 	});
-	describe('_isLegalElement()', function () {
-		it('should allow legal children', function () {
-			expect(malebranche._isLegalElement('path')).toBe(true);
-			expect(malebranche._isLegalElement('text')).toBe(true);
-			expect(malebranche._isLegalElement('use')).toBe(true);
-			expect(malebranche._isLegalElement('rect')).toBe(true);
-			expect(malebranche._isLegalElement('circle')).toBe(true);
-			expect(malebranche._isLegalElement('ellipse')).toBe(true);
-			expect(malebranche._isLegalElement('line')).toBe(true);
-			expect(malebranche._isLegalElement('polyline')).toBe(true);
-			expect(malebranche._isLegalElement('polygon')).toBe(true);
+	xdescribe('serialize()', function () {
+		var pathArray= [ 
+			{ code: 'M', command: 'moveto', x: 3, y: 7 },
+  		{ code: 'L', command: 'lineto', x: 5, y: -6 },
+  		{ code: 'L', command: 'lineto', x: 1, y: 7 },
+  		{ code: 'L', command: 'lineto', x: 100, y: -0.4 },
+  		{ code: 'm', command: 'moveto', relative: true, x: -10, y: 10 },
+  		{ code: 'l', command: 'lineto', relative: true, x: 10, y: 0 } ];
+		it('should return path', function () {
+			expect(malebranche.serialize(pathArray)).toBe('M3,7 5-6 L1,7 1e2-.4 m-10,10 l10,0');
 		});
-		it('should disallow illegal children', function () {
-			expect(malebranche._isLegalElement('div')).toBe(false);
+	});
+	describe('convert()', function () {
+
+		var svg = "<svg><clipPath><path d='M3,7 L5,-6 L1,7 L100,-0.4 m-10,10 l10,0'/></clipPath></svg>";
+		malebranche.convert(svg);
+	});
+	xdescribe('_forEachClipPath()', function () {
+		var spy;
+		var obj = {
+			foo  : [
+				{ clipPath : { name : 'alpha' } },
+				{ bar : {
+						blah : {
+							moo : 'moo', clipPath : { name : 'beta' }
+						}
+					}
+				}
+			],
+			clipPath : []
+		};
+		beforeEach(function () {
+			spy = jasmine.createSpy();
+		});
+		it('should invoke callback for each clipPath element',function (){
+			malebranche._forEachClipPath(obj, spy);
+			expect(spy.calls.count()).toBe(3);
 		});
 	});
 });
