c133385bd4483f5db5b2c77e97dd7ef846a774b3
Richard Hunter <richardhunter@Richards-MBP.default>
2015-10-25 16:36:21 +0000

    private function to test for legal child elements

--- index.js
+++ index.js
@@ -1,4 +1,45 @@
+var parse  = require('svg-path-parser');
+
+function test() {
+
+	var result = parse('M3,7 5-6 L1,7 1e2-.4 m-10,10 l10,0');
+	console.log(result);
+
+}
+test();
+//  convert array of commands back to string
+//  surprised this isn't in svg path parser?
+exports.toPath = function(commands) {
+	//  commands is an array of commands produced by svg pathparser.
+	return '';
+
+};
+//  http://www.w3.org/TR/SVG11/masking.html#EstablishingANewClippingPath
+exports.allowedElementTypes = [
+	'path', 'text', 'use', 'rect', 'circle', 'ellipse', 'line', 'polyline', 'polygon'
+];
+
+exports.isLegalElement = function(element) {
+	return this.allowedElementTypes.indexOf(element) != -1;
+}
+
 exports.printModuleName = function () {
 	console.log('this is Malebranche');
 };
 exports.name = 'malebranche';
+
+exports.convert = function (fragment, initialWidth, initialHeight) {
+
+	/*
+		arguments are a string representing an svg clippath, the initial width of the box containing 
+the path and the initial height. The convert function should take all the coords specified within the path and make them proportionate to the width or height and then return the new clippath as a string.
+
+Should throw an error if the input is badly formed. the user is responsible for passing a correctly formed clippath.
+
+	*/
+
+}
+
+exports.convertFile = function (svgFile) {
+	//  should take an svg file and convert all clip paths in it
+}
--- spec/main.spec.js
+++ spec/main.spec.js
@@ -1,8 +1,27 @@
+
+
 describe('Malebranche tests', function () {
+
 	var malebranche = require('../index.js');
-	describe('malebranche.name', function () {
+	describe('name', function () {
 		it('should return module name', function () {
 			expect(malebranche.name).toBe('malebranche');
+		});
+	});
+	describe('isLegalElement()', function () {
+		it('should allow legal children', function () {
+			expect(malebranche.isLegalElement('path')).toBe(true);
+			expect(malebranche.isLegalElement('text')).toBe(true);
+			expect(malebranche.isLegalElement('use')).toBe(true);
+			expect(malebranche.isLegalElement('rect')).toBe(true);
+			expect(malebranche.isLegalElement('circle')).toBe(true);
+			expect(malebranche.isLegalElement('ellipse')).toBe(true);
+			expect(malebranche.isLegalElement('line')).toBe(true);
+			expect(malebranche.isLegalElement('polyline')).toBe(true);
+			expect(malebranche.isLegalElement('polygon')).toBe(true);
+		});
+		it('should disallow illegal children', function () {
+			expect(malebranche.isLegalElement('div')).toBe(false);
 		});
 	});
 });
