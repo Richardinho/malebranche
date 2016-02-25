8a371ab004b17aca811ffb41ab5762f7f03556e0
Richard Hunter <richard@richardhunter.co.uk>
2016-02-25 14:51:16 +0000

    reference length command line parameter

--- bin/cli.js
+++ bin/cli.js
@@ -3,11 +3,12 @@
 var malebranche = require('../malebranche.js');
 
 var srcFile = process.argv[2];
+var referenceLength = process.argv[3];
 
-if(srcFile) {
-	malebranche.main(srcFile);
+if(srcFile && referenceLength) {
+	malebranche.main(srcFile, referenceLength);
 } else {
 	throw {
-		message : 'you must supply a source file'
+		message : 'you must supply a source file and a reference length'
 	};
 }
--- malebranche.js
+++ malebranche.js
@@ -12,7 +12,6 @@ var arrayFromPoints = malebrancheUtils.arrayFromPoints;
 var readFile = malebrancheIO.readFile;
 var writeFile = malebrancheIO.writeFile;
 
-var refsize = 960;
 
 /*
 	The idea here is to change the coords within svgObj in place.
@@ -42,103 +41,103 @@ function forEachClipPath(svgObj, callback) {
 }
 
 // what if size is greater than 1?
-function handleCircle(refsize, circle) {
+function handleCircle(reflength, circle) {
 	var circleData = circle['$'];
-	circleData.r = parseInt(circleData.r, 10) / refsize;
-	circleData.cy = parseInt(circleData.cy, 10) / refsize;
-	circleData.cx = parseInt(circleData.cx, 10) / refsize;
+	circleData.r = parseInt(circleData.r, 10) / reflength;
+	circleData.cy = parseInt(circleData.cy, 10) / reflength;
+	circleData.cx = parseInt(circleData.cx, 10) / reflength;
 };
 
-function handleCommand (refsize, command) {
+function handleCommand (reflength, command) {
 	switch(command.code){
 	case 'M':
-		command.x = command.x / refsize;
-		command.y = command.y / refsize;
+		command.x = command.x / reflength;
+		command.y = command.y / reflength;
 		break;
 	case 'm':
-		command.x = command.x / refsize;
-		command.y = command.y / refsize;
+		command.x = command.x / reflength;
+		command.y = command.y / reflength;
 		break;
 	case 'L':
-		command.x = command.x / refsize;
-		command.y = command.y / refsize;
+		command.x = command.x / reflength;
+		command.y = command.y / reflength;
 		break;
 	case 'l':
-		command.x = command.x / refsize;
-		command.y = command.y / refsize;
+		command.x = command.x / reflength;
+		command.y = command.y / reflength;
 		break;
 	case 'V':
-		command.y = command.y / refsize;
+		command.y = command.y / reflength;
 		break;
 	case 'v':
-		command.y = command.y / refsize;
+		command.y = command.y / reflength;
 		break;
 	case 'H':
-		command.x = command.x / refsize;
+		command.x = command.x / reflength;
 		break;
 	case 'h':
-		command.x = command.x / refsize;
+		command.x = command.x / reflength;
 		break;
 	case 'C':
-		command.x = command.x / refsize;
-		command.y = command.y / refsize;
-		command.x1 = command.x1 / refsize;
- 		command.y1 = command.y1 / refsize;
-		command.x2 = command.x2 / refsize;
- 		command.y2 = command.y2 / refsize;
+		command.x = command.x / reflength;
+		command.y = command.y / reflength;
+		command.x1 = command.x1 / reflength;
+ 		command.y1 = command.y1 / reflength;
+		command.x2 = command.x2 / reflength;
+ 		command.y2 = command.y2 / reflength;
 		break;
 	case 'c':
-		command.x = command.x / refsize;
-		command.y = command.y / refsize;
-		command.x1 = command.x1 / refsize;
- 		command.y1 = command.y1 / refsize;
-		command.x2 = command.x2 / refsize;
- 		command.y2 = command.y2 / refsize;
+		command.x = command.x / reflength;
+		command.y = command.y / reflength;
+		command.x1 = command.x1 / reflength;
+ 		command.y1 = command.y1 / reflength;
+		command.x2 = command.x2 / reflength;
+ 		command.y2 = command.y2 / reflength;
 		break;
 	case 'S':
-		command.x = command.x / refsize;
-		command.y = command.y / refsize;
-		command.x2 = command.x2 / refsize;
- 		command.y2 = command.y2 / refsize;
+		command.x = command.x / reflength;
+		command.y = command.y / reflength;
+		command.x2 = command.x2 / reflength;
+ 		command.y2 = command.y2 / reflength;
 		break;
 	case 's':
-		command.x = command.x / refsize;
-		command.y = command.y / refsize;
-		command.x2 = command.x2 / refsize;
- 		command.y2 = command.y2 / refsize;
+		command.x = command.x / reflength;
+		command.y = command.y / reflength;
+		command.x2 = command.x2 / reflength;
+ 		command.y2 = command.y2 / reflength;
 		break;
 	case 'Q':
-		command.x = command.x / refsize;
-		command.y = command.y / refsize;
-		command.x1 = command.x1 / refsize;
-		command.y1 = command.y1 / refsize;
+		command.x = command.x / reflength;
+		command.y = command.y / reflength;
+		command.x1 = command.x1 / reflength;
+		command.y1 = command.y1 / reflength;
 		break;
 	case 'q':
-		command.x = command.x / refsize;
-		command.y = command.y / refsize;
-		command.x1 = command.x1 / refsize;
-		command.y1 = command.y1 / refsize;
+		command.x = command.x / reflength;
+		command.y = command.y / reflength;
+		command.x1 = command.x1 / reflength;
+		command.y1 = command.y1 / reflength;
 		break;
 	case 'T':
-		command.x = command.x / refsize;
-		command.y = command.y / refsize;
+		command.x = command.x / reflength;
+		command.y = command.y / reflength;
 		break;
 	case 't':
-		command.x = command.x / refsize;
-		command.y = command.y / refsize;
+		command.x = command.x / reflength;
+		command.y = command.y / reflength;
 		break;
 	case 'A':
-		command.x = command.x / refsize;
-		command.y = command.y / refsize;
-		command.rx = command.rx / refsize;
-		command.ry = command.ry / refsize;
+		command.x = command.x / reflength;
+		command.y = command.y / reflength;
+		command.rx = command.rx / reflength;
+		command.ry = command.ry / reflength;
 		// do I need to do x-axis-rotation?
 		break;
 	case 'a':
-		command.x = command.x / refsize;
-		command.y = command.y / refsize;
-		command.rx = command.rx / refsize;
-		command.ry = command.ry / refsize;
+		command.x = command.x / reflength;
+		command.y = command.y / reflength;
+		command.rx = command.rx / reflength;
+		command.ry = command.ry / reflength;
 		// do I need to do x-axis-rotation?
 		break;
 	default :
@@ -147,54 +146,54 @@ function handleCommand (refsize, command) {
 }
 
 
-function handlePath(refsize, path) {
+function handlePath(reflength, path) {
 	var pathString = path['$']['d'];
 	var pathCommands = parsePath(pathString);
-	pathCommands.forEach(handleCommand.bind(null, refsize));
+	pathCommands.forEach(handleCommand.bind(null, reflength));
 	path['$']['d'] = pathBuilder.build(pathCommands);
 }
 
-function handleRectangle(refsize, rectangle) {
+function handleRectangle(reflength, rectangle) {
 	var rectData = rectangle['$'];
-	rectData.x = parseInt(rectData.x , 10) / refsize;
-	rectData.y = parseInt(rectData.y , 10) / refsize;
-	rectData.width = parseInt(rectData.width , 10) / refsize;
-	rectData.height = parseInt(rectData.height , 10) / refsize;
+	rectData.x = parseInt(rectData.x , 10) / reflength;
+	rectData.y = parseInt(rectData.y , 10) / reflength;
+	rectData.width = parseInt(rectData.width , 10) / reflength;
+	rectData.height = parseInt(rectData.height , 10) / reflength;
 }
 
-function handlePolygon(refsize, polygon) {
+function handlePolygon(reflength, polygon) {
 	var pointsArray = arrayFromPoints(polygon['$']['points']);
 	var transformedPointsArray = pointsArray.map(function (point) {
-		return point / refsize;
+		return point / reflength;
 	});
 	polygon['$']['points'] = transformedPointsArray.join(' ');
 }
 
-function handleEllipse(refsize, ellipse) {
+function handleEllipse(reflength, ellipse) {
 	var ellipseData = ellipse['$'];
-	ellipseData.cx = parseInt(ellipseData.cx, 10) / refsize;
-	ellipseData.cy = parseInt(ellipseData.cy, 10) / refsize;
-	ellipseData.rx = parseInt(ellipseData.rx, 10) / refsize;
-	ellipseData.ry = parseInt(ellipseData.ry, 10) / refsize;
+	ellipseData.cx = parseInt(ellipseData.cx, 10) / reflength;
+	ellipseData.cy = parseInt(ellipseData.cy, 10) / reflength;
+	ellipseData.rx = parseInt(ellipseData.rx, 10) / reflength;
+	ellipseData.ry = parseInt(ellipseData.ry, 10) / reflength;
 }
 
 //  this will change the coords of the clip path from absolute to fractional
-function changeClipPathCoords(clipPath) {
+function changeClipPathCoords(reflength, clipPath) {
 	var circles = clipPath[0]['circle'];
 	var paths = clipPath[0]['path'];
 	var rectangles = clipPath[0]['rect'];
 	var ellipses = clipPath[0]['ellipse'];
 	var polygons = clipPath[0]['polygon'];
 
-	if(circles) circles.forEach(handleCircle.bind(null, refsize));
-	if(paths) paths.forEach(handlePath.bind(null, refsize));
-	if(rectangles) rectangles.forEach(handleRectangle.bind(null, refsize));
-	if(polygons) polygons.forEach(handlePolygon.bind(null, refsize));
-	if(ellipses) ellipses.forEach(handleEllipse.bind(null, refsize));
+	if(circles) circles.forEach(handleCircle.bind(null, reflength));
+	if(paths) paths.forEach(handlePath.bind(null, reflength));
+	if(rectangles) rectangles.forEach(handleRectangle.bind(null, reflength));
+	if(polygons) polygons.forEach(handlePolygon.bind(null, reflength));
+	if(ellipses) ellipses.forEach(handleEllipse.bind(null, reflength));
 }
 
-function convertCoords(result) {
-	forEachClipPath(result, changeClipPathCoords);
+function convertCoords(reflength, result) {
+	forEachClipPath(result, changeClipPathCoords.bind(null, reflength));
 	return result;
 }
 
@@ -202,11 +201,11 @@ function errorHandler(error) {
 	console.log('error', error);
 }
 
-exports.main = function(srcFile) {
+exports.main = function(srcFile, referenceLength) {
 
 	readFile(srcFile)
 		.then(parseStringIntoJs)
-		.then(convertCoords)
+		.then(convertCoords.bind(null, referenceLength))
 		.then(serializeJSIntoString)
 		.then(writeFile)
 		.catch(errorHandler);
