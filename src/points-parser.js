aef4c20669b9f20599694b00fab86d7ae9c6f96b
Richard Hunter <richard@richardhunter.co.uk>
2020-06-18 22:33:01 +0100

    refactored most code into malebranche-lib. This package only consists of the CLI

--- src/points-parser.js
+++
@@ -1,7 +0,0 @@
-function arrayFromPoints(pointsString) {
-	return pointsString.trim().split(/[ ,]+/).map(function (el) {
-		return parseInt(el, 10);
-	});
-}
-
-exports.arrayFromPoints = arrayFromPoints;
