9dfb67288a77f348d59194186657845e37474b0f
Richard Hunter <richard@richardhunter.co.uk>
2020-06-18 23:02:22 +0100

    fixing indentation on io file

--- src/malebranche-io.js
+++ src/malebranche-io.js
@@ -1,27 +1,27 @@
 var fs = require('fs')
 
 function readFile(srcFile) {
-	return new Promise(function(resolve, reject){
-		fs.readFile(srcFile, function(err, data) {
-			if(err) {
-				reject(err);
-			} else {
-				resolve(data);
-			}
-		});
-	});
+  return new Promise(function(resolve, reject){
+    fs.readFile(srcFile, function(err, data) {
+      if(err) {
+        reject(err);
+      } else {
+        resolve(data);
+      }
+    });
+  });
 }
 
 function writeFile(name, contents) {
-	return new Promise(function(resolve, reject){
-		fs.writeFile(name || 'destination.svg', contents, function(err) {
-			if(err) {
-				reject(err);
-			} else {
-				resolve();
-			}
-	  });
-	});
+  return new Promise(function(resolve, reject){
+    fs.writeFile(name || 'destination.svg', contents, function(err) {
+      if(err) {
+        reject(err);
+      } else {
+        resolve();
+      }
+    });
+  });
 }
 
 exports.readFile = readFile;
